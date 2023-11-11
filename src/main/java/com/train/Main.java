fb50aaa6b9befb6cc260a0443ba84133f49f13c2
jacklee8899 <jack_lee88@hotmail.com.tw>
2023-11-11 22:00:53 +0800

    Test commit

--- src/main/java/com/train/Main.java
+++ src/main/java/com/train/Main.java
@@ -4,8 +4,8 @@ package com.train;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+        Tester tester = new Tester();
+
         System.out.println("Hello and welcome!");
     }
 }
